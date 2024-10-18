f6845b284bdf3816d670acc53b63f5eb0bbca674
Chris Brusie <brusie@wustl.edu>
2024-10-18 12:51:14 -0400

    traffic lights done

--- ledlab/traffic.py
+++ ledlab/traffic.py
@@ -14,23 +14,34 @@ GPIO.setup(26,GPIO.OUT)
 
 start_time = time.time()
 cur_time = start_time
-while(True):
+for j in range(2):
     GPIO.output(8,True)
+    GPIO.output(18,True)
     GPIO.output(10,True)
+    GPIO.output(22,True)
     time.sleep(3)
     GPIO.output(10,False)
+    GPIO.output(22,False)
     time.sleep(4)
     GPIO.output(8,False)
+    GPIO.output(18,False)
     GPIO.output(12,True)
-    time.sleep(3)
-    for i in range(6):
-        on = True
-        GPIO.output(12,on)
-        on = not on
+    GPIO.output(24,True)
+    time.sleep(1.5)
+    for i in range(3):
+        GPIO.output(12,False)
+        GPIO.output(24,False)
         time.sleep(0.25)
+        GPIO.output(12,True)
+        GPIO.output(24,True)
+        time.sleep(0.25)
+    GPIO.output(12,False)
+    GPIO.output(24,False)
     GPIO.output(16,True)
+    GPIO.output(26,True)
     time.sleep(5)
     GPIO.output(16,False)
+    GPIO.output(26,False)
         
 
 
