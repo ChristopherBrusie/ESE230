dfc33f64baee4f59cfa5e2e5c760fedc74c7e404
ChristopherBrusie <brusie@wustl.edu>
2024-10-27 13:40:11 -0400

    all done!

--- ledlab/8b.py
+++ ledlab/8b.py
@@ -2,29 +2,41 @@ import RPi.GPIO as GPIO
 import time
 import numpy as np
 
-GPIO.setwarnings(False)
+# Set up GPIO mode
 GPIO.setmode(GPIO.BOARD)
-GPIO.cleanup()
+GPIO.setwarnings(False)
 
-GPIO.setup(8, GPIO.OUT)
-GPIO.setup(10, GPIO.OUT)
-GPIO.setup(12, GPIO.OUT)
-GPIO.setup(16, GPIO.OUT)
-GPIO.setup(18, GPIO.OUT)
-GPIO.setup(22, GPIO.OUT)
-GPIO.setup(24, GPIO.OUT)
-GPIO.setup(26, GPIO.OUT)
-GPIO.setup(32, GPIO.OUT)
-GPIO.setup(36, GPIO.IN)
+# Define GPIO pins
+button_pin = 36
 
-pins = (8, 10, 12, 16, 18, 22, 24, 26, 32)
-pins_arr = np.array([8, 10, 12, 16, 18, 22, 24, 26, 32])
+# Set up button and LEDs
+GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-while True:
-    if GPIO.input(36):
-        for i in range(9):
-            GPIO.output(int(pins_arr[i]), True)
-            time.sleep(0.1)
-            GPIO.output(int(pins_arr[i]), False)
-    else:
-        GPIO.output(pins, False);
+pins = np.array([8, 10, 12, 16, 18, 22, 24, 32, 26])
+pins_list = (8, 10, 12, 16, 18, 22, 24, 32, 26)
+GPIO.setup(pins_list, GPIO.OUT)
+# Initialize state variables
+blinking = False
+
+def button_callback(channel):
+    global blinking
+    blinking = not blinking
+
+# Set up event detection for the button
+GPIO.add_event_detect(button_pin, GPIO.FALLING, callback=button_callback, bouncetime=300)
+
+try:
+    while True:
+        if blinking:
+            for i in range(9):
+                GPIO.output(int(pins[i]), True)
+                time.sleep(0.1)
+                GPIO.output(int(pins[i]), False)
+        else:
+            GPIO.output(pins_list, GPIO.LOW)
+
+except KeyboardInterrupt:
+    print("\nExiting program.")
+
+finally:
+    GPIO.cleanup()
--- ledlab/button_test_ignore.py
+++ ledlab/button_test_ignore.py
@@ -1,18 +1,21 @@
 import RPi.GPIO as GPIO
 import time
-
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.cleanup()
+GPIO.setup(36, GPIO.IN)
 pressed = False
 counter = 0
 current = time.time()
-last_pressed = False;
+last_pressed = True;
 
 while(True):
     if(time.time() > current + .05):
-        pressed = GPIO.input(16)
+        pressed = GPIO.input(36)
         if pressed != last_pressed and last_pressed == False:
             counter += 1
+            print(counter)
         last_pressed = pressed
-        print(counter)
         current = time.time()
 
 
