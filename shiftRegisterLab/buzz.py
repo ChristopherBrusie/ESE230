24558e6e60e31debbd942b2fc978e8075ce81e5a
ChristopherBrusie <brusie@wustl.edu>
2024-11-06 14:35:02 -0500

    hi

--- shiftRegisterLab/buzz.py
+++ shiftRegisterLab/buzz.py
@@ -3,21 +3,26 @@ import time
 
 switchPin = 16
 ledPin = 12
-buzzPin = 13
+buzzPin = 18
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.cleanup()
+
 
 GPIO.setup(switchPin, GPIO.IN)
 GPIO.setup(ledPin, GPIO.OUT)
 GPIO.setup(buzzPin, GPIO.OUT)
-
+GPIO.output(buzzPin,True)
 while True:
     if(GPIO.input(switchPin)):
         GPIO.output(ledPin,True)
         GPIO.output(buzzPin,True)
         time.sleep(1)
+        print('hi')
     else:
+        print("Low")
         GPIO.output(ledPin,False)
         GPIO.output(buzzPin,False)
+
+
+GPIO.cleanup()
